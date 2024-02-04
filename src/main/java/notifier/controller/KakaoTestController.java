59739bf24a0f490ee2b4765ffbc6c0faa5c22d79
chjung99 <cksgh1168@gmail.com>
2024-02-04 20:02:27 +0900

    chore: response body

--- src/main/java/notifier/controller/KakaoTestController.java
+++ src/main/java/notifier/controller/KakaoTestController.java
@@ -3,12 +3,14 @@ package notifier.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hello")
 public class KakaoTestController {
     @PostMapping
+    @ResponseBody
     public String hello(){
         return "hello";
     }
