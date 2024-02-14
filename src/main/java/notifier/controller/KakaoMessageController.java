f229b44d3286d39f384364b21bf62b8f9d3d7dec
chjung99 <cksgh1168@gmail.com>
2024-02-15 00:28:06 +0900

    chore: change menu text style

--- src/main/java/notifier/controller/KakaoMessageController.java
+++ src/main/java/notifier/controller/KakaoMessageController.java
@@ -26,16 +26,18 @@ public class KakaoMessageController {
     public KakaoSkillResponseDto create(@RequestBody KakaoSkillPayloadDto kakaoSkillPayloadDto){
         String skillName = kakaoSkillPayloadDto.getAction().getName();
         Optional<Menu> menu = getTodayMenu(skillName, kakaoSkillPayloadDto);
-        KakaoSkillResponseDto response = generateResponse(menu);
+        KakaoSkillResponseDto response = generateResponse(skillName, menu);
         return response;
     }
 
-    private KakaoSkillResponseDto generateResponse(Optional<Menu> menu) {
+    private KakaoSkillResponseDto generateResponse(String mealTime, Optional<Menu> menu) {
         KakaoSkillResponseDto response = new KakaoSkillResponseDto();
         SimpleText simpleText = new SimpleText();
+
+        String menuTextsTitle = getMenuTextsTitle(mealTime);
         String concatenatedMenuTexts = concatenateMenuItems(menu);
 
-        simpleText.setText(concatenatedMenuTexts);
+        simpleText.setText(menuTextsTitle +"\n\n" + concatenatedMenuTexts);
         Output output = new Output();
         output.setSimpleText(simpleText);
         
@@ -49,19 +51,28 @@ public class KakaoMessageController {
         return response;
     }
 
+    private String getMenuTextsTitle(String mealTime) {
+        if(mealTime.equals("lunchMenu")){
+            return "\uD83C\uDF7D\uFE0F 오늘의 점심 \uD83C\uDF5A";
+        } else if (mealTime.equals("dinnerMenu")) {
+            return "\uD83C\uDF7D\uFE0F 오늘의 저녁 \uD83C\uDF5A";
+        }
+        return "mealTime ERROR";
+    }
+
     private String concatenateMenuItems(Optional<Menu> menuOptional) {
         StringBuilder concatenatedItems = new StringBuilder();
 
         menuOptional.ifPresent(menu -> {
             for (String item : menu.getItems()) {
-                concatenatedItems.append(item).append(", ");
+                concatenatedItems.append("✔\uFE0F ").append(item).append("\n");
             }
         });
 
         // 마지막 쉼표 및 공백 제거
-        if (concatenatedItems.length() > 0) {
-            concatenatedItems.delete(concatenatedItems.length() - 2, concatenatedItems.length());
-        }
+//        if (concatenatedItems.length() > 0) {
+//            concatenatedItems.delete(concatenatedItems.length() - 2, concatenatedItems.length());
+//        }
 
         return concatenatedItems.toString();
     }
